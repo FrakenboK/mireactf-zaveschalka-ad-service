3fd91c210bdd9d78177b6e8f5f19d7d6d6a48df1
FrakenboK <xxxeromant@gmail.com>
2024-05-06 03:35:34 +0300

    Update checker and service

--- checker/will_lib.py
+++ checker/will_lib.py
@@ -27,9 +27,9 @@ class WillLib:
             return 0
         
     def _check_profile(self, content: str, username: str, email: str, phone: str):
-        self.c.assert_in(username, content, f'Failed to get profile data for {self.api_url} (username)')
-        self.c.assert_in(email, content, f'Failed to get profile data for {self.api_url} (email)')
-        self.c.assert_in(phone, content, f'Failed to get profile data for {self.api_url} (phone)')
+        self.c.assert_in(username, content, f'Failed to get profile data for (username)')
+        self.c.assert_in(email, content, f'Failed to get profile data (email)')
+        self.c.assert_in(phone, content, f'Failed to get profile data (phone)')
 
     def register(self, session: requests.Session, username: str, password: str, email: str, phone: str):
         resp = session.post(f'{self.api_url}/register.php', data={
@@ -38,7 +38,8 @@ class WillLib:
             'phone': phone,
             'email': email 
         })
-        self.c.assert_in('profile.php', resp.url, f'Failed to register for {self.api_url}')
+        self.c.assert_in('profile.php', resp.url, f'Failed to register')
+        self.c.assert_eq(200, resp.status_code, 'Failed to register')
         self._check_profile(resp.text, username, email, phone)
 
 
@@ -47,7 +48,8 @@ class WillLib:
             'login': username,
             'password': password
         })
-        self.c.assert_in('profile.php', resp.url, f'Failed to login for {self.api_url}')
+        self.c.assert_in('profile.php', resp.url, f'Failed to login')
+        self.c.assert_eq(200, resp.status_code, 'Failed to login')
         self._check_profile(resp.text, username, email, phone)
     
     def create_will(self, session: requests.Session, title: str, will: str, username_to_share: str):
@@ -56,9 +58,12 @@ class WillLib:
             'will': will,
             'username0': username_to_share
         })
-        self.c.assert_nin('create_will.php', resp.url, 'Failed to create will')
+        self.c.assert_in('will.php?id=', resp.url, 'Failed to create will')
+        print(resp.status_code)
+        self.c.assert_eq(200, resp.status_code, 'Failed to create will')
         return self._get_will_id(resp.url)
     
     def check_will(self, session: requests.Session, will_id: str, flag: str, is_shared: bool = False):
         resp = session.get(f'{self.api_url}/will.php?id={will_id}')
         self.c.assert_in(flag, resp.text, f"Failed to get {'shared ' if is_shared else ''}will", Status.CORRUPT)
+        self.c.assert_eq(200, resp.status_code, 'Failed to create will')
